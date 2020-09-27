bae2423804e179a71309a9946b9beedfb023d015
Laurits <warupdk@gmail.com>
2020-09-27 14:41:15 +0200

    VerifyTripeWithOpening header

---
+++ aby3/sh3/Sh3Verifier.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "Sh3Encryptor.h"
+
+namespace aby3
+{
+    class Sh3Verifier {
+        public:
+            void init(u64 partyIdx, block prevSeed, block nextSeed, u64 buffSize = 256) {mEncryptor.init(partyIdx, prevSeed, nextSeed, buffSize); mPartyIdx = partyIdx; }; 
+
+            bool verifyTripleWithOpening(CommPkg& comm, const std::array<si64&, 3> xyz, const std::array<si64&, 3> abc);
+            Sh3Task verifyTripleWithOpening(Sh3Task dep, const std::array<si64&, 3> xyz, const std::array<si64&, 3> abc, bool& dest);
+            
+            template<Decimal D>
+            bool verifyTripleWithOpening(CommPkg& comm, const std::array<sf64<D>&, 3> xyz, const std::array<sf64<D>&, 3> abc);
+            template<Decimal D>
+            Sh3Task verifyTripleWithOpening(Sh3Task dep, const std::array<sf64<D>&, 3> xyz, const std::array<sf64<D>&, 3> abc, bool& dest);
+
+            bool verifyTripleWithOpening(CommPkg& comm, const std::array<sb64&, 3> xyz, const std::array<sb64&, 3> abc);
+            Sh3Task verifyTripleWithOpening(Sh3Task dep, const std::array<sb64&, 3> xyz, const std::array<sb64&, 3> abc, bool& dest);
+
+            bool verifyTripleWithOpening(CommPkg& comm, const std::array<si64Matrix&, 3> xyz, const std::array<si64Matrix&, 3> abc);
+            Sh3Task verifyTripleWithOpening(Sh3Task dep, const std::array<si64Matrix&, 3> xyz, const std::array<si64Matrix&, 3> abc, bool& dest);
+
+            template<Decimal D>
+            bool verifyTripleWithOpening(CommPkg& comm, const std::array<sf64Matrix<D>&, 3> xyz, const std::array<sf64Matrix<D>&, 3> abc);
+            template<Decimal D>
+            Sh3Task verifyTripleWithOpening(Sh3Task dep, const std::array<sf64Matrix<D>&, 3> xyz, const std::array<sf64Matrix<D>&, 3> abc, bool& dest) ;
+
+            bool verifyTripleWithOpening(CommPkg& comm, const std::array<sbMatrix&, 3> xyz, const std::array<sbMatrix&, 3> abc);
+            Sh3Task verifyTripleWithOpening(Sh3Task dep, const std::array<sbMatrix&, 3> xyz, const std::array<sbMatrix&, 3> abc, bool& dest);
+
+
+        u64 mPartyIdx;
+        Sh3Encryptor mEncryptor;
+    };
+}
