637dc200ae8a4e55ef8f9da849eaf61a1deae9a3
Laurits <warupdk@gmail.com>
2020-10-19 09:23:45 +0200

    verifyTriple using another header

--- aby3/sh3/Sh3Verifier.h
+++ aby3/sh3/Sh3Verifier.h
@@ -1,44 +1,91 @@
 #pragma once
+
 #include "Sh3Encryptor.h"
 
-namespace aby3
-{
+namespace aby3 {
     class Sh3Verifier {
-        public:
-            void init(u64 partyIdx, block prevSeed, block nextSeed, u64 buffSize = 256) {mEncryptor.init(partyIdx, prevSeed, nextSeed, buffSize); mPartyIdx = partyIdx; }; 
+    public:
+        void init(u64 partyIdx, block prevSeed, block nextSeed, u64 buffSize = 256) {
+            mEncryptor.init(partyIdx, prevSeed, nextSeed, buffSize);
+            mPartyIdx = partyIdx;
+        };
 
-            bool verifyTripleWithOpening(CommPkg& comm, const std::array<si64, 3>& abc);
-            Sh3Task verifyTripleWithOpening(Sh3Task dep, const std::array<si64, 3>& abc, bool& dest);
-            
-            template<Decimal D>
-            bool verifyTripleWithOpening(CommPkg& comm, const std::array<sf64<D>, 3>& abc);
-            template<Decimal D>
-            Sh3Task verifyTripleWithOpening(Sh3Task dep,  const std::array<sf64<D>, 3>& abc, bool& dest);
+        bool verifyTripleWithOpening(CommPkg &comm, const std::array<si64, 3> &abc);
 
-            bool verifyTripleWithOpening(CommPkg& comm, const std::array<sb64, 3>& abc);
-            Sh3Task verifyTripleWithOpening(Sh3Task dep, const std::array<sb64, 3>& abc, bool& dest);
+        Sh3Task verifyTripleWithOpening(Sh3Task dep, const std::array<si64, 3> &abc, bool &dest);
 
-            bool verifyTripleWithOpening(CommPkg& comm, const std::array<si64Matrix, 3>& abc);
-            Sh3Task verifyTripleWithOpening(Sh3Task dep, const std::array<si64Matrix, 3>& abc, bool& dest);
+        template<Decimal D>
+        bool verifyTripleWithOpening(CommPkg &comm, const std::array<sf64<D>, 3> &abc);
 
-            template<Decimal D>
-            bool verifyTripleWithOpening(CommPkg& comm, const std::array<sf64Matrix<D>, 3>& abc);
-            template<Decimal D>
-            Sh3Task verifyTripleWithOpening(Sh3Task dep, const std::array<sf64Matrix<D>, 3>& abc, bool& dest) ;
+        template<Decimal D>
+        Sh3Task verifyTripleWithOpening(Sh3Task dep, const std::array<sf64<D>, 3> &abc, bool &dest);
 
-            bool verifyTripleWithOpening(CommPkg& comm, const std::array<sbMatrix, 3>& abc);
-            Sh3Task verifyTripleWithOpening(Sh3Task dep, const std::array<sbMatrix, 3>& abc, bool& dest);
+        bool verifyTripleWithOpening(CommPkg &comm, const std::array<sb64, 3> &abc);
 
-            bool compareView(CommPkg& comm, i64& x);
-            Sh3Task compareView(Sh3Task dep, i64& x, bool& dest);
+        Sh3Task verifyTripleWithOpening(Sh3Task dep, const std::array<sb64, 3> &abc, bool &dest);
 
-            bool verifyTripleUsingAnother(CommPkg& comm, const std::array<si64, 3>& xyz, const std::array<si64, 3>& abc);
-            bool verifyTripleUsingAnother(CommPkg& comm, const std::array<sb64, 3>& xyz, const std::array<sb64, 3>& abc);
+        bool verifyTripleWithOpening(CommPkg &comm, const std::array<si64Matrix, 3> &abc);
 
-            Sh3Task verifyTripleUsingAnother(Sh3Task dep, const std::array<si64, 3>& xyz, const std::array<si64, 3>& abc, bool& dest);
-            Sh3Task verifyTripleUsingAnother(Sh3Task dep, const std::array<sb64, 3>& xyz, const std::array<sb64, 3>& abc, bool& dest);
+        Sh3Task verifyTripleWithOpening(Sh3Task dep, const std::array<si64Matrix, 3> &abc, bool &dest);
 
-            u64 mPartyIdx;
-            Sh3Encryptor mEncryptor;
+        template<Decimal D>
+        bool verifyTripleWithOpening(CommPkg &comm, const std::array<sf64Matrix<D>, 3> &abc);
+
+        template<Decimal D>
+        Sh3Task verifyTripleWithOpening(Sh3Task dep, const std::array<sf64Matrix<D>, 3> &abc, bool &dest);
+
+        bool verifyTripleWithOpening(CommPkg &comm, const std::array<sbMatrix, 3> &abc);
+
+        Sh3Task verifyTripleWithOpening(Sh3Task dep, const std::array<sbMatrix, 3> &abc, bool &dest);
+
+        bool compareView(CommPkg &comm, i64 &x);
+
+        Sh3Task compareView(Sh3Task dep, i64 &x, bool &dest);
+
+        bool verifyTripleUsingAnother(CommPkg &comm, const std::array<si64, 3> &xyz, const std::array<si64, 3> &abc);
+
+        bool verifyTripleUsingAnother(CommPkg &comm, const std::array<sb64, 3> &xyz, const std::array<sb64, 3> &abc);
+
+        template<Decimal D>
+        bool
+        verifyTripleUsingAnother(CommPkg &comm, const std::array<sf64<D>, 3> &xyz, const std::array<sf64<D>, 3> &abc);
+
+        bool verifyTripleUsingAnother(CommPkg &comm, const std::array<si64Matrix, 3> &xyz,
+                                      const std::array<si64Matrix, 3> &abc);
+
+        template<Decimal D>
+        bool verifyTripleUsingAnother(CommPkg &comm, const std::array<sf64Matrix<D>, 3> &xyz,
+                                      const std::array<sf64Matrix<D>, 3> &abc);
+
+        bool
+        verifyTripleUsingAnother(CommPkg &comm, const std::array<sbMatrix, 3> &xyz, const std::array<sbMatrix, 3> &abc);
+
+
+        Sh3Task verifyTripleUsingAnother(Sh3Task dep, const std::array<si64, 3> &xyz, const std::array<si64, 3> &abc,
+                                         bool &dest);
+
+        Sh3Task verifyTripleUsingAnother(Sh3Task dep, const std::array<sb64, 3> &xyz, const std::array<sb64, 3> &abc,
+                                         bool &dest);
+
+        template<Decimal D>
+        Sh3Task
+        verifyTripleUsingAnother(Sh3Task dep, const std::array<sf64<D>, 3> &xyz, const std::array<sf64<D>, 3> &abc,
+                                 bool &dest);
+
+        Sh3Task verifyTripleUsingAnother(Sh3Task dep, const std::array<si64Matrix, 3> &xyz,
+                                         const std::array<si64Matrix, 3> &abc,
+                                         bool &dest);
+
+        template<Decimal D>
+        Sh3Task verifyTripleUsingAnother(Sh3Task dep, const std::array<sf64Matrix<D>, 3> &xyz,
+                                         const std::array<sf64Matrix<D>, 3> &abc,
+                                         bool &dest);
+
+        Sh3Task
+        verifyTripleUsingAnother(Sh3Task dep, const std::array<sbMatrix, 3> &xyz, const std::array<sbMatrix, 3> &abc,
+                                 bool &dest);
+
+        u64 mPartyIdx;
+        Sh3Encryptor mEncryptor;
     };
 }
